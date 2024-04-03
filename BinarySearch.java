5fb8303f7b53f65dd9918b3563dc26eacbe7e093
Nikhil Kesharwani <114168065+Nikhil7354@users.noreply.github.com>
2024-04-03 16:14:59 +0530

    Binary Search

---
+++ BinarySearch.java
@@ -0,0 +1,27 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,12,16,23,45,67,89};
+        int target = 16;
+        System.out.println(bS(arr,target));
+
+    }
+
+    static int bS(int[] arr,int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
